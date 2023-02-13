470bec816d97a53644f56fc8ecfef6fc6c9aa6dd
Javohir <javohircomuity@gmail.com>
2023-02-13 09:35:31 +0500

    Initial project

---
+++ src/main/java/com/pos/monitoring/PosMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.pos.monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosMonitoringApplication.class, args);
+	}
+
+}
