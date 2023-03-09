88945ec9a0e3f7772d526a4d511b114674414267
Muhammadjafar <ibook2025com@gmail.com>
2023-03-09 11:42:10 +0500

    service initial

---
+++ src/main/java/com/pos/monitoring/services/impl/MachineHistoryServiceImpl.java
@@ -0,0 +1,16 @@
+package com.pos.monitoring.services.impl;
+
+import com.pos.monitoring.repositories.MachineHistoryRepository;
+import com.pos.monitoring.services.MachineHistoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MachineHistoryServiceImpl implements MachineHistoryService {
+    private final MachineHistoryRepository machineHistoryRepository;
+    @Override
+    public void synchronize() {
+
+    }
+}
